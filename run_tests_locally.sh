4446f79a2673eda39d4718a910e76967a2b48139
Paul Anderson <pauleanderson@gmail.com>
2022-09-26 09:50:02 -0700

    update

--- run_tests_locally.sh
+++ run_tests_locally.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-name=$(cat "student_repo.txt")
+repo=$(cat "student_repo.txt")
+echo $name
 for file in `ls *.ipynb`; do
   echo "Testing $file"
   name="${file%.*}"
-  pytest -vv --diff-symbols ../csc-466-student/tests/test_$name.py
+  pytest -vv --diff-symbols ../$repo/tests/test_$name.py
 done;
 
